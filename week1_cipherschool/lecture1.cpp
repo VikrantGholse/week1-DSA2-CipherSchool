ddf6fcd15063430e7ecbc2bbfec00bf3da07784e
Vikrant Gholse <vikrantgholse23@gmail.com>
2023-06-23 13:31:41 +0530

    initial commit

---
+++ week1_cipherschool/lecture1.cpp
@@ -0,0 +1,32 @@
+ #include<bits/stdc++.h>
+
+ using namespace std;
+ 
+ void mergeSortedArray(vector<int> &a , int m , vector<int> &b , int n)
+ {
+    int i = m-1 , j = n-1 , k=m+n-1;
+    while (i >= 0 && j >= 0)
+    {
+        if (a[i] > b[j] )
+        {
+            a[k] = a[i];
+            i--;
+        }
+        else{
+            a[k] = a[j];
+            j--;
+        }
+        k--;
+    }
+    while (j >= 0)
+    {
+        a[k]=b[j];
+        j--;
+        k--;
+    }
+ }
+ int main()
+ {
+    int a[5]={1,3,4},b[2]={6,9};
+    mergeSortedArray();
+ }
